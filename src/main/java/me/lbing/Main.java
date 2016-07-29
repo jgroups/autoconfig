e823deb780376a0be806e5385cf9048ffb4b2c0b
JeremyKing <jgroups@163.com>
2016-07-29 13:55:07 +0800

    multi config files

--- src/main/java/me/lbing/Main.java
+++ src/main/java/me/lbing/Main.java
@@ -6,38 +6,25 @@ import java.io.InputStream;
 
 
 public class Main {
-	public static void main( String[] args ) throws Exception {
-		InputStream is = Main.class.getClassLoader().getResourceAsStream( "my-config.properties" );
-		if ( is == null ) {
-			System.err.println( "Can not load config resource my-config.properties in classpath" );
-		} else {
-			Properties prop = new Properties();
-			prop.load( is );
-			is.close();
-			for( String key : prop.stringPropertyNames() ) {
-				String value = prop.getProperty( key );
-				if ( value != null ) {
-					System.out.printf( "%s = %s %n", key, value );
-				}
-			}
-		}
-		testConfig();
-	}
+    public static void main(String[] args) throws Exception {
+        testMyConfig("my-config.properties", "Can not load config resource my-config.properties in classpath");
+        testMyConfig("config.properties", "Can not load config resource config.properties in classpath");
+    }
 
-	private static void testConfig() throws IOException {
-		InputStream is = Main.class.getClassLoader().getResourceAsStream( "config.properties" );
-		if ( is == null ) {
-			System.err.println( "Can not load config resource config.properties in classpath" );
-		} else {
-			Properties prop = new Properties();
-			prop.load( is );
-			is.close();
-			for( String key : prop.stringPropertyNames() ) {
-				String value = prop.getProperty( key );
-				if ( value != null ) {
-					System.out.printf( "%s = %s %n", key, value );
-				}
-			}
-		}
-	}
+    private static void testMyConfig(String name, String x) throws IOException {
+        InputStream is = Main.class.getClassLoader().getResourceAsStream(name);
+        if (is == null) {
+            System.err.println(x);
+        } else {
+            Properties prop = new Properties();
+            prop.load(is);
+            is.close();
+            for (String key : prop.stringPropertyNames()) {
+                String value = prop.getProperty(key);
+                if (value != null) {
+                    System.out.printf("%s = %s %n", key, value);
+                }
+            }
+        }
+    }
 }
