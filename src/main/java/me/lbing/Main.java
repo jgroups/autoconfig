01a964f65eba62b176fc0f735a21cb0742d707d6
JeremyKing <jgroups@163.com>
2016-07-29 13:51:21 +0800

    multi config files

--- src/main/java/me/lbing/Main.java
+++ src/main/java/me/lbing/Main.java
@@ -1,11 +1,30 @@
 package me.lbing;
 
+import java.io.IOException;
 import java.util.Properties;
 import java.io.InputStream;
 
 
 public class Main {
 	public static void main( String[] args ) throws Exception {
+		InputStream is = Main.class.getClassLoader().getResourceAsStream( "my-config.properties" );
+		if ( is == null ) {
+			System.err.println( "Can not load config resource my-config.properties in classpath" );
+		} else {
+			Properties prop = new Properties();
+			prop.load( is );
+			is.close();
+			for( String key : prop.stringPropertyNames() ) {
+				String value = prop.getProperty( key );
+				if ( value != null ) {
+					System.out.printf( "%s = %s %n", key, value );
+				}
+			}
+		}
+		testConfig();
+	}
+
+	private static void testConfig() throws IOException {
 		InputStream is = Main.class.getClassLoader().getResourceAsStream( "config.properties" );
 		if ( is == null ) {
 			System.err.println( "Can not load config resource config.properties in classpath" );
